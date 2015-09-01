f681af58fb7d3092f6f6fca8a24c8885683a1f99
zigdog97 <zigdog97@cox.net>
2015-09-01 10:21:15 -0400

    Initial Commit

---
+++ WouldYouRather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WouldYouRather
+//
+//  Created by zach bundarin on 9/1/15.
+//  Copyright (c) 2015 Zach Bundarin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
